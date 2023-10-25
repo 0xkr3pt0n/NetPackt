9d48a15a99ba05b72fa4fe4bff961274b4080ca3
0xkr3pt0n <alarab.akm.akm@gmail.com>
2023-10-25 17:10:36 +0300

    gitignore update

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netimpact',
         'USER': 'postgres',
-        'PASSWORD': 'Dodo@1',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
