f815897bd6fc66454bc74d9cb2d378bc5d6e9174
0xkr3pt0n <alarab.akm.akm@gmail.com>
2023-10-30 21:13:25 +0200

    enhancment of gui and validation

--- myapp/core/views.py
+++ myapp/core/views.py
@@ -2,38 +2,23 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import SignupForm
 from django.contrib.auth import logout
+from django.contrib import messages
 
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
 
         if form.is_valid():
-            
             form.save()
-
             return redirect('/login/')
+            
+        else:
+            messages.error(request, 'Invalid, check requirement fields.')
+
     else:
         form = SignupForm()
 
-    return render(request, 'core/register.html', {
-        'form': form
-    })
-def signup(request):
-    if request.method == 'POST':
-        form = SignupForm(request.POST)
-
-        if form.is_valid():
-            
-            form.save()
-
-            return redirect('/login/')
-    else:
-        form = SignupForm()
-
-    return render(request, 'core/register.html', {
-        'form': form
-    })
-
+    return render(request, 'core/register.html', {'form': form})
 
 @login_required
 def home(request):
