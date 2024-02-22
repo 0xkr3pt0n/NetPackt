06fe3d78f2aeb8c28876b2358f035310aa6c8ee9
0xk3rypt0n <alarab.akm.akm@gmail.com>
2024-02-22 11:53:08 +0200

    deleting old branch

--- myapp/netscan/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path,include
-from . import views
-
-urlpatterns = [
-    path('scans/', views.scans, name="scans"),
-    path('report/<int:report_id>/', views.report, name="report_detils"),
-    path('delete_report/<int:report_id>/', views.delete_report, name='delete_report'),
-]
