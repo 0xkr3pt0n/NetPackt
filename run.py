5073d8da7d2c991f7712815ff4c4bcf5b625d321
0xk3rypt0n <alarab.akm.akm@gmail.com>
2024-03-29 15:54:42 +0200

    modifiying run.py to update db

--- run.py
+++ run.py
@@ -12,10 +12,13 @@ class runAPP:
         self.proccess = []
 
     def run(self):
+        self.updatedb()
+        time.sleep(10)
         self.run_server()
         time.sleep(5)
         self.backP = self.process_tasks()
-
+    def updatedb(self):
+        subprocess.Popen(["python", "database.py"])
     def run_server(self):
         subprocess.Popen(["python", "manage.py", "runserver"])
 
