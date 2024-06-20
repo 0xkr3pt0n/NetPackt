c181702f76841549c7b24aef6c867eae773d7570
Ali Sayed <107375683+Alisayedra@users.noreply.github.com>
2024-06-21 00:59:23 +0300

    change some css
    alter the home.css
    alter the dashboard.css and html

---
+++ scripts.js
@@ -0,0 +1,10 @@
+/*!
+    * Start Bootstrap - SB Admin v7.0.7 (https://startbootstrap.com/template/sb-admin)
+    * Copyright 2013-2023 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
+    */
+    // 
+// Scripts
+// 
+
+
